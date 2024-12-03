8b1b8b9a134ad671132096bd5656a2057f9cc332
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-12-03 23:05:01 +0500

    hard level completed

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ computer_choice = random.randint(0,100)
 guess = 0
 
 def easy_level():
+    attempts = 10
     print("You have 10 chances!")
     for chance in range(0,10):
         guess = int(input("Guess a Number: "))
@@ -16,10 +17,34 @@ def easy_level():
             print("You Win!")
             break
         elif guess < computer_choice:
-            print("Too Low")    
+            print("Too Low") 
+            attempts -= 1   
+            print(f"You have {attempts} attempts left")
         elif guess > computer_choice:
             print("Too High")
+            attempts -= 1  
+            print(f"You have {attempts} attempts left")
+    if attempts == 0:
+        print("You lost!")
 
+def hard_level():
+    attempts = 5
+    print("You have 5 chances!")
+    for chance in range(0,5):
+        guess = int(input("Guess a Number: "))
+        if guess == computer_choice:
+            print("You Win!")
+            break
+        elif guess < computer_choice:
+            print("Too Low") 
+            attempts -= 1 
+            print(f"You have {attempts} attempts left")
+        elif guess > computer_choice:
+            print("Too High")
+            attempts -= 1  
+            print(f"You have {attempts} attempts left")
+    if attempts == 0:
+        print("You lost!")
      
 
 
@@ -27,5 +52,5 @@ def easy_level():
 level = input("Select level: 'easy' or 'hard': ")
 if level == 'easy':
     easy_level()
-# elif level == 'hard':
-#     hard_level()    
+elif level == 'hard':
+    hard_level()    
