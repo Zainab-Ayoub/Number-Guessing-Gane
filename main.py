40851aa7e21183609f1ebbb44081050a3c2e665d
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-12-03 22:36:23 +0500

    easy level complete

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-enemies = 1
+import random
+from art import logo
+print(logo)
 
-def increase_enemies():
-    enemies = 2
-    print(f"Enemies inside function: {enemies}")
+print("Welcome to the Number Guessing Game!")
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")    
+computer_choice = random.randint(0,100)
+
+guess = 0
+
+def easy_level():
+    print("You have 10 chances!")
+    for chance in range(0,10):
+        guess = int(input("Guess a Number: "))
+        if guess == computer_choice:
+            print("You Win!")
+            break
+        elif guess < computer_choice:
+            print("Too Low")    
+        elif guess > computer_choice:
+            print("Too High")
+
+     
+
+
+
+level = input("Select level: 'easy' or 'hard': ")
+if level == 'easy':
+    easy_level()
+# elif level == 'hard':
+#     hard_level()    
